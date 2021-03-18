0d5ae64ec9af5996211f5a003c2c18a8ec49d523
Vimal <vj6978@gmail.com>
2021-03-18 21:09:39 +0000

    First commit

---
+++ src/main/java/com/example/cyan/Cyan/CyanApplication.java
@@ -0,0 +1,13 @@
+package com.example.cyan.Cyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cyan/Cyan/CyanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cyan.Cyan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
